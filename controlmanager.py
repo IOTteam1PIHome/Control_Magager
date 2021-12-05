408fcdd3a7c8d97bbe1897d7a4491d6adf1025a3
Sonny8659 <cksghd321@gmail.com>
2021-12-06 02:05:55 +0900

    first commit

---
+++ controlmanager.py
@@ -0,0 +1,26 @@
+import os
+import paho.mqtt.client as mqtt
+import sys
+from datetime import datetime
+import requests
+import json
+
+topic = "#"
+server = "34.193.131.206" #EC2 address
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with RC : " + str(rc))
+    client.subscribe(topic)
+
+def on_message(client, userdata, msg):
+    if(msg.payload == "emer"):
+        os.system("python ./controlmanagerTexting.py") #Texting.py dir 
+    elif(msg.payload == "weather") :
+        os.system("python ./controlmanager/weather.py") #weather.py dir
+
+client =mqtt.Client()
+client.connect(server, 1883, 60)
+client.on_connect = on_connect
+client.on_message = on_message
+client.subscribe("#")
+client.loop_forever()
